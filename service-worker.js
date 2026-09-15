f7dcedf810059dc119283f1139743970c21870b1
fatch1621 <165745968+fatch1621@users.noreply.github.com>
2026-09-15 10:48:35 +0700

    update pop up

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 // =========================================================
 // SERVICE WORKER - KeuanganKu PWA
 // =========================================================
-const CACHE_NAME = 'keuanganku-v3.1.2';
+const CACHE_NAME = 'keuanganku-v3.1.3';
 
 // File yang di-cache saat pertama kali install
 const PRECACHE_URLS = [
