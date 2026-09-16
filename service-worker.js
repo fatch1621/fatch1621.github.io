7667ee4581670f5b8ed1024b3e98388cc6ba479c
fatch1621 <165745968+fatch1621@users.noreply.github.com>
2026-09-16 13:57:38 +0700

    3.1.4

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 // =========================================================
 // SERVICE WORKER - KeuanganKu PWA
 // =========================================================
-const CACHE_NAME = 'keuanganku-v3.1.3';
+const CACHE_NAME = 'keuanganku-v3.1.4';
 
 // File yang di-cache saat pertama kali install
 const PRECACHE_URLS = [
